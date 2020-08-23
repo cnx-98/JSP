b4779aa3076e2b804e0f0944398fc9af7e90646f
cwh_gol <176_gol@163.com>
2020-08-23 22:32:13 +0800

    第二次修改

--- src/main/java/com/bjpowernode/github/Test.java
+++ src/main/java/com/bjpowernode/github/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("爱你呦，娜姐");
         System.out.println("        念昔");
+        System.out.println("        哈哈");
     }
 }
